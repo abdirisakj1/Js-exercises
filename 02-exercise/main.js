5a1619e3ede19c8a4f2b3d8d27a0c1fab5a6f6db
abdirisakj1 <abdirisakjaran@gmail.com>
2025-12-28 11:56:30 +0300

    submited exercise 2

---
+++ 02-exercise/main.js
@@ -0,0 +1,28 @@
+let a = 15;
+let b = "15";
+
+// comparison operators
+
+// equal to
+console.log(a == b);
+
+// not equal to
+console.log(a != b);
+
+// strictly to
+console.log(a === b);
+
+// strictly not equal
+console.log(a !== b);
+// greater then 
+console.log(a > b);
+
+// less then
+
+console.log(a < b);
+
+// greater then? or equal
+console.log(a >= b);
+
+// less then ? or equal
+console.log(a <= b);

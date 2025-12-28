fe44ca57ee705f9fab8e5d2985b465ee4dc92f7d
abdirisakj1 <abdirisakjaran@gmail.com>
2025-12-28 18:05:11 +0300

    exercise 4 submited

---
+++ 04-exercise/main.js
@@ -0,0 +1,38 @@
+// decalaration function
+
+function add (x,y) {
+    return x + y;
+} 
+add(10,8);
+
+let r1 = add(20,7);
+console.log(r1);
+
+let r2 = add(30,6);
+console.log(r2);
+
+let r3 = add(40,5);
+console.log(r3);
+
+let r4 = add(50,4);
+console.log(r4);
+
+let r5 = add(60,3);
+console.log(r5);
+
+
+// function expresion
+const sum = function(a,b) {
+    return a+b;
+}
+let r6 = add(21,2);
+console.log(r6);
+
+let r7 = add(22,2);
+console.log(r7);
+
+let r8 = add(33,2);
+console.log(r8);
+
+let r9 = add(54,2);
+console.log(r9);

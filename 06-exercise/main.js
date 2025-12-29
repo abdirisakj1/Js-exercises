fcf4bb44ff657f64ac97e2f17d91e1d1f7ebffe1
abdirisakj1 <abdirisakjaran@gmail.com>
2025-12-29 14:34:40 +0300

    submited exercise 6

---
+++ 06-exercise/main.js
@@ -0,0 +1,10 @@
+// array
+let colors = ["blue","green","yellow"];
+
+// print array
+console.log(colors);
+
+// single index
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);

e9ea8bb6c1213400813e3cb00c5bb5fcd447dd5c
abdirisakj1 <abdirisakjaran@gmail.com>
2025-12-29 17:28:07 +0300

    submited exercise 10

---
+++ 10-exercise/main.js
@@ -0,0 +1,31 @@
+let liabrary = [
+    {
+        title: "manafaay",
+        author: "Mohamed",
+        year:    1991
+    }, 
+    {
+        title: "jiraalkay",
+        author: "Dr waliid",
+        year:    2014
+    }, 
+
+    {
+        title: "furaha ganacsiga",
+        author: "mohamed",
+        year: 2018
+    }, 
+    {
+        title: "jiraalkay3",
+        author: "dr waliid",
+        year: 2023
+    }
+];
+
+console.log(liabrary);
+
+// title of first book
+console.log(liabrary[0].title);
+
+// author of second book
+console.log(liabrary[1].author);

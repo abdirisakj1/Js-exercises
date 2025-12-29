5027eb690f7fb42f6685c7bb3395f2671a331d80
abdirisakj1 <abdirisakjaran@gmail.com>
2025-12-29 15:16:56 +0300

    submited exercise 7

---
+++ 07-exercise/main.js
@@ -0,0 +1,16 @@
+let colors = ["blue","green","yellow"];
+
+// new color 
+console.log("After push");
+
+colors.push("red");
+console.log(colors);
+
+//removing first color
+console.log("Ater shift");
+
+colors.shift();
+console.log(colors);
+
+// list number of array
+console.log(colors.length);

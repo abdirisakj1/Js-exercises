06b77b7af205d485a9f9399ea46e3f31447642b6
abdirisakj1 <abdirisakjaran@gmail.com>
2025-12-30 11:38:17 +0300

    submited exercise 12

--- 11-exercise/main.js
+++ 11-exercise/main.js
@@ -1,4 +1,5 @@
 // for loop
+// 1 - 50
 
 for (let i = 1; i <= 50; i ++) {
     console.log(i);
---
+++ 12-exercise/main.js
@@ -0,0 +1,9 @@
+// for loop array
+
+let fruits = ["Apple", "banana", "cherry"];
+
+for (let i = 0; i < fruits.length; i ++) {
+
+    //  array of fruit
+    console.log(fruits[i]);
+}

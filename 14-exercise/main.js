6c3282ba2b1bfa26617d54d61416927ecc2b7be5
abdirisakj1 <abdirisakjaran@gmail.com>
2025-12-30 13:47:46 +0300

    submited exercise 14

---
+++ 14-exercise/main.js
@@ -0,0 +1,8 @@
+// do while loop user can enter number greater then 10
+
+let userNumber;
+
+do {
+    userNumber = prompt("enter number!");
+    console.log("you entered correct number! " + userNumber);
+} while(userNumber < 10)

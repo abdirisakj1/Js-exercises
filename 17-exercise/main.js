43c6696b866125c975a887f02cbf9de852084ecb
abdirisakj1 <abdirisakjaran@gmail.com>
2025-12-31 10:42:56 +0300

    submited exercise 17

---
+++ 17-exercise/main.js
@@ -0,0 +1,11 @@
+let temperature = 20;
+
+if (temperature < 0) {
+    console.log("Very cold! 🥶");
+} else if(temperature < 15) {
+    console.log("cold 💧");
+} else if(temperature < 25) {
+    console.log("Warm 🏞️");
+} else{
+    console.log("Hot 🥵");
+}

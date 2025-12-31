aa898eab9422890a156a48725a7b5e67f1a6e047
abdirisakj1 <abdirisakjaran@gmail.com>
2025-12-31 18:47:55 +0300

    submited exercise 21

--- 20-exercise/main.js
+++ 20-exercise/main.js
@@ -1,6 +1,9 @@
 // array method map
 
+
 let fruits = ["Apple","banana","Cherry"];
+
+// each string length
 
 let Myfruits = fruits.map((fruit) => fruit.length);
 console.log(Myfruits);
---
+++ 21-exercise/main.js
@@ -0,0 +1,5 @@
+let numbers = [10,20,30,40,50];
+
+const multiply = numbers.reduce((result,num) => result * num,1) 
+
+console.log(multiply);

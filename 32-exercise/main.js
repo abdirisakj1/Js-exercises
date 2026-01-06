72045f7f4bc822e2e2c622a611a4f4a5564f99d8
abdirisakj1 <abdirisakjaran@gmail.com>
2026-01-06 20:16:17 +0300

    submited exercise 32

---
+++ 32-exercise/main.js
@@ -0,0 +1,12 @@
+// DOM Selector QuerySelector AND QuerySelectorAll
+const title = document.querySelector('#head');
+console.log(title);
+
+const work = document.querySelector('.career');
+console.log(work);
+const pargraphs = document.querySelectorAll('.pargraph');
+console.log(pargraphs);
+
+const btns = document.querySelectorAll('button');
+
+console.log(btns);

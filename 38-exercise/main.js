6360d8cd751490ce4b74f9c16e17154dbc0a3177
abdirisakj1 <abdirisakjaran@gmail.com>
2026-01-28 11:25:49 +0300

    submited exercise 38

---
+++ 38-exercise/main.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.querySelector(".toggle-button");
+const navbar = document.querySelector(".navbar");
+
+toggleBtn.addEventListener("click", function() {
+    navbar.classList.toggle("active")
+})
